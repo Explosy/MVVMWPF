a64ed54773daeba90b4cbb0cc06acf2295952046
Explosy <devotion1996@gmail.com>
2022-09-26 20:24:08 +0300

    Add Command

---
+++ MVVMWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using MVVMWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMWPF.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Book selectedBook;
+        public ObservableCollection<Book> Books { get; set; }
+        
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                OnPropertyChanged("SelectedBook");
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            Books = new ObservableCollection<Book>
+            {
+                new Book {Title="WPF Unleashed", Author="Adam Natan", Year=2012 },
+                new Book {Title="F# for Machine Learning", Author="Sudipta Mukherjee", Year=2016 },
+                new Book {Title="F# for Fun and Profit", Author="Scott Wlaschin", Year=2015 },
+                new Book {Title="Learning C# by Developing Games with Unity 3D", Author="Terry Norton", Year=2013 }
+            };
+        }
+
+        #region Command
+        private BaseCommand addCommand;
+        public BaseCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                (addCommand = new BaseCommand(obj =>
+                {
+                    Book Book = new Book();
+                    Books.Insert(0, Book);
+                    SelectedBook = Book;
+                }));
+            }
+        }
+        #endregion Command
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
